a068c43d8b0ba8fccae248e9c19dceae752560e0
doneyen <62895297+doneyen@users.noreply.github.com>
2020-04-17 01:07:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,6 +6,4 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.168.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/Leo-Jo-My/luci-app-vssr-plus package/luci-app-vssr-plus
-git clone https://github.com/Leo-Jo-My/my package/my
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
